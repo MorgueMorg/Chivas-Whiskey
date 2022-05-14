4133b4105c24f15ada86c7ac8f543f295221a387
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-05-14 19:36:32 +0600

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import AdminContext from "./contexts/AdminContext";
 import ClientContext from "./contexts/ClientContext";
 import Navigation from "./Navigation";
+
 import "bootstrap/dist/css/bootstrap.css";
-import AuthContextProvider from "./contexts/ResetPassword";
-// import 'swiper/swiper.css'
-// import 'swiper/swiper.scss'
-// import 'swiper/swiper.less'
-// import "swiper/swiper-bundle.css"
-// import "swiper/css/swiper.css"
 import "swiper/css/bundle";
-import { ChakraProvider } from "@chakra-ui/react";
-import { UserAuthContextProvider } from "./contexts/UserAuthContext";
 
 const App = () => {
   return (
     <ClientContext>
       <AdminContext>
-        <UserAuthContextProvider>
-          <AuthContextProvider>
-          {/* <ChakraProvider> */}
             <Navigation />
-          {/* </ChakraProvider> */}
-          </AuthContextProvider>
-        </UserAuthContextProvider>
       </AdminContext>
     </ClientContext>
   );
--- src/Navigation.js
+++ src/Navigation.js
@@ -3,9 +3,6 @@ import {
   BrowserRouter,
   Route,
   Routes,
-  Redirect,
-  useLocation,
-  Navigate,
 } from "react-router-dom";
 import Catalog from "./pages/Catalog";
 import Navbar from "./components/Navbar";
@@ -17,79 +14,26 @@ import MainPage from "./pages/MainPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProductDetails from "./pages/ProductDetails";
 import Favorites from "./pages/Favorites";
-import SignUp from "./pages/SignUp";
-import LogIn from "./pages/LogIn";
-import AuthProvider from "./contexts/AuthProvider";
-import { UserAuthContextProvider } from "./contexts/UserAuthContext";
 import Footer from "./components/Footer";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage";
-import { useAuth } from "./contexts/ResetPassword";
-import { ChakraProvider } from "@chakra-ui/react";
 
 const Navigation = () => {
   return (
-    // <UserAuthContextProvider>
-      <AuthProvider>
-          {/* <ChakraProvider> */}
-            <BrowserRouter>
-              <Navbar />
-              <Routes>
-                <Route path="/admin-panel/add" element={<AddProductPage />} />
-                <Route path="/admin-panel" element={<AdminPage />} />
-                <Route
-                  path="/admin-panel/edit/:id"
-                  element={<EditProductPage />}
-                />
-                <Route path="/" element={<MainPage />} />
-                <Route path="/catalog" element={<Catalog />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/details/:id" element={<ProductDetails />} />
-                <Route path="/favorites" element={<Favorites />} />
-                <Route path="/*" element={<NotFoundPage />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/login" element={<LogIn />} />
-                <Route
-                  path="/forgot-password"
-                  element={<ForgotPasswordPage />}
-                />
-                <Route path="/reset-password" element={<ResetPasswordPage />} />
-              </Routes>
-              <Footer />
-            </BrowserRouter>
-          {/* </ChakraProvider> */}
-      </AuthProvider>
-    // </UserAuthContextProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/admin-panel/add" element={<AddProductPage />} />
+          <Route path="/admin-panel" element={<AdminPage />} />
+          <Route path="/admin-panel/edit/:id" element={<EditProductPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/details/:id" element={<ProductDetails />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
   );
 };
-
-// function ProtectedRoute(props) {
-//   const { currentUser } = useAuth()
-//   const { path } = props
-//   console.log('path', path)
-//   const location = useLocation()
-//   console.log('location state', location.state)
-
-//   if (
-//     path === '/forgot-password' ||
-//     path === '/reset-password'
-//   ) {
-//     return currentUser ? (
-//       <Navigate to={location.state?.from ?? '/profile'} />
-//     ) : (
-//       <Route {...props} />
-//     )
-//   }
-//   return currentUser ? (
-//     <Route {...props} />
-//   ) : (
-//     <Navigate
-//       to={{
-//         pathname: '/login',
-//         state: { from: path },
-//       }}
-//     />
-//   )
-// }
 
 export default Navigation;
